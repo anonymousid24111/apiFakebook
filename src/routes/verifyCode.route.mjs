5cde145cc82404b8636b3629d8cf9beeee0b296a
anonymousid24111 <anonymousid2411@gmail.com>
2020-10-06 20:36:59 +0700

    Revert "init"
    This reverts commit 19ca6bd437bad9957f8ae5941ffedd03d64d8413.

--- src/routes/verifyCode.route.mjs
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-router.get("/get", (req, res) => {
-  return res.status(200).json("this is /user page");
-});
-
-module.exports = router;
