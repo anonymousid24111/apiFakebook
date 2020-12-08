681d8f2e61f41555cc16d4cbf581182f0eb77baf
Nguyen Duc Giang 20173079 <giang.nd173079@sis.hust.edu.vn>
2020-12-08 22:39:03 +0700

    update search

--- src/controllers/six.controller.js
+++ src/controllers/six.controller.js
@@ -16,11 +16,15 @@ const statusCode = require("../constants/statusCode.constant.js");
 const statusMessage = require("../constants/statusMessage.constant.js");
 
 const search = async (req, res) => {
-    const { token, user_id, keyword, index, count } = req.query;
+    var {  user_id, keyword, index, count } = req.query;
     const { _id } = req.userDataPass;
     // check params
     try {
-        if (!index || !count || !user_id|| !keyword) {
+        if(!index||!count||index<0||count<0){
+            index=0;
+            count=20;
+        }
+        if (!user_id|| !keyword) {
             throw Error("params");
         }
         await User.findByIdAndUpdate(_id,{
@@ -39,7 +43,10 @@ const search = async (req, res) => {
         var postData1 =await Post.find({$or: [
             { described: new RegExp(keyword, "i") },
             { described: new RegExp(keyword.replace(" ", "|"), "i") }
-        ]});
+        ]}).populate({
+            path: "author",
+            select:"username avatar"
+        });
         return res.status(200).json({
             code: statusCode.OK,
             message: statusMessage.OK,
@@ -47,17 +54,17 @@ const search = async (req, res) => {
         })
     } catch (error) {
         if (error.message == "params") {
-            return res.status(200).json({
+            return res.status(500).json({
                 code: statusCode.PARAMETER_VALUE_IS_INVALID,
                 message: statusMessage.PARAMETER_VALUE_IS_INVALID
             })
         } else if (error.message == "nodata") {
-            return res.status(200).json({
+            return res.status(500).json({
                 code: statusCode.NO_DATA_OR_END_OF_LIST_DATA,
                 message: statusMessage.NO_DATA_OR_END_OF_LIST_DATA
             })
         } else {
-            return res.status(200).json({
+            return res.status(500).json({
                 code: statusCode.UNKNOWN_ERROR,
                 message: statusMessage.UNKNOWN_ERROR
             })
